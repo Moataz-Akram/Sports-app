f05c78f5a8c5dd7d7c034b0d8d59e9e5ba7ed984
Moataz-Akram <61953238+Moataz-Akram@users.noreply.github.com>
2021-04-22 04:47:22 +0200

    alamofire, URLs

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'Sports app' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -8,7 +8,8 @@ target 'Sports app' do
   # Pods for Sports app
   
   pod 'SDWebImage', '~> 5.0'
-  pod 'ReachabilitySwift'
+  pod 'Alamofire', '~> 5.2'
+
 
   target 'Sports appTests' do
     inherit! :search_paths
--- Sports app/LeaguesViewController.swift
+++ Sports app/LeaguesViewController.swift
@@ -6,84 +6,80 @@
 //
 
 import UIKit
+import Alamofire
+import SDWebImage
 
 class LeaguesViewController: UITableViewController {
+    
+    var leagues = [League]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        AF.request(URLs.allLeaguesURL).validate().responseDecodable(of: LeagueAPI.self) { (response) in
+            switch response.result {
+            case .success( _):
+                print("success")
+                guard let leagues = response.value?.leagues else { return }
+                print("leauges count \(leagues.count)")
+                for leauge in leagues {
+                    if leauge.strSport == "Soccer" {
+                        var comingLeauge = leauge
+                        comingLeauge = self.getLeaugeData(comingLeauge)
+                        print(comingLeauge.strLogo ?? "append")
+//                        var comingLeauge = leauge
+//                        comingLeauge.strYoutube = "hello"
+                        self.leagues.append(comingLeauge)
+                    }
+                }
+                print("leauges table count \(self.leagues.count)")
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getLeaugeData(_ leauge : League)->League{
+        let LeagueDetailURL = String("\(URLs.LeagueDetail)\(leauge.idLeague!)")
+        var newLeauge = leauge
+        AF.request(LeagueDetailURL).validate().responseDecodable(of: LeagueDetailAPI.self) { (response) in
+            switch response.result {
+            case .success( _):
+                let leauges = response.value?.leagues
+//                print(leauges?[0].strLogo ?? "")
+                newLeauge.strYoutube = leauges?[0].strYoutube
+                newLeauge.strLogo = leauges?[0].strLogo
+//                print(newLeauge.strLogo ?? "")
+          case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        return newLeauge
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return leagues.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "leaguesCell", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leaguesCell", for: indexPath) as! LeaugeCustomCell
+        cell.leaugeName.text = leagues[indexPath.row].strLeague
+        print("from cell \(leagues[indexPath.row].strLogo ?? "")")
+        if let imgStr = leagues[indexPath.row].strLogo{
+            cell.leaugeImage.sd_setImage(with: URL(string: imgStr), placeholderImage: UIImage(named: "placeholder"))
+        }
 
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
