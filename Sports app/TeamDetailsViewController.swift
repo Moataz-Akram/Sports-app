d9649c91de5e0dceae3c56793c51e368b7132313
MarwaEbrahem <marwa15shanab@gmail.com>
2021-04-24 09:46:28 -0500

    view teamDetails data in view controller

--- Sports app/TeamDetailsViewController.swift
+++ Sports app/TeamDetailsViewController.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
-
+import SDWebImage
 class TeamDetailsViewController: UIViewController {
+
+    @IBOutlet weak var dsec: UILabel!
+    @IBOutlet weak var badgeImg: UIImageView!
+    @IBOutlet weak var stadiumImg: UIImageView!
+    @IBOutlet weak var teamName: UILabel!
+    @IBOutlet weak var leaguesName: UILabel!
+    @IBOutlet weak var stadiumName: UILabel!
+    @IBOutlet weak var countryName: UILabel!
     let teamID : Int? = 133604
     var teamDetails : [[String: String?]] = [[String: String?]]()
     let teamDetailsViewModel = TeamDetailsViewModel( )
@@ -33,8 +41,15 @@ class TeamDetailsViewController: UIViewController {
     func onSuccessUpdateView(){
         
         teamDetails = teamDetailsViewModel.teamDetailsData
-        var x = teamDetails[0]
+        let x = teamDetails[0]
         print(x["strTeam"]!!)
+        dsec.text = x["strDescriptionEN"]!!
+        teamName.text = x["strTeam"]!!
+        stadiumName.text = x["strStadium"]!!
+        countryName.text = x["strCountry"]!!
+        leaguesName.text = x["strLeague"]!!
+        badgeImg.sd_setImage(with: URL(string: x["strTeamBadge"]!!), placeholderImage: UIImage(named:"1"))
+        stadiumImg.sd_setImage(with: URL(string: x["strStadiumThumb"]!!), placeholderImage: UIImage(named:"1"))
         
     }
     
